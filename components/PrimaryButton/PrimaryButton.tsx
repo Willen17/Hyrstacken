bd4f4b58a787e9b76360fab7a29cac3309f0783f
matsegard <matsegard@outlook.com>
2022-11-25 09:14:48 +0100

    pages

--- components/PrimaryButton/PrimaryButton.tsx
+++ components/PrimaryButton/PrimaryButton.tsx
@@ -1,11 +1,9 @@
 const PrimaryButton = () => {
   return (
-    <div className="h-screen flex justify-center items-center text-8xl text-blue-500 px-10">
-      <div>
-        <p className="font-nunito font-extralight">
-          Låna verktyg istället för att köpa nytt
-        </p>
-      </div>
+    <div className="PrimaryButton">
+      <button className="btn btn-primary rounded-full text-lightGray">
+        Button
+      </button>
     </div>
   );
 };
---
+++ pages/profile/[id].tsx
@@ -0,0 +1,5 @@
+const ProfilePage = () => {
+  return <div className=""></div>;
+};
+
+export default ProfilePage;
