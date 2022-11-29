55510e528df4eed88e9a1b59691061de7bac118e
William Saar <williamsaar@outlook.com>
2022-11-29 09:58:38 +0100

    added icon and started working on ui for cretaeotem form

--- pages/createItem.tsx
+++ pages/createItem.tsx
@@ -1,29 +1,26 @@
-import { useForm } from "react-hook-form";
-import { itemSchema } from "../lib/schemas";
-import { zodResolver } from '@hookform/resolvers/zod';
-import { z } from "zod";
 import { InferGetServerSidePropsType, NextPage } from "next";
 import prisma from "../lib/prisma";
 import ItemForm from "../components/Forms/ItemForm";
 import Head from "next/head";
+import AddIcon from "../assets/add.svg";
 
 export async function getServerSideProps() {
   const categories = await prisma.category.findMany({
     select: {
       id: true,
       name: true,
-    }
+    },
   });
   return {
     props: {
       categories,
     },
   };
-};
+}
 
-const CreateItem: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({categories}) => {
-  
-
+const CreateItem: NextPage<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({ categories }) => {
   return (
     <>
       <Head>
@@ -31,15 +28,17 @@ const CreateItem: NextPage<InferGetServerSidePropsType<typeof getServerSideProps
         <meta name="description" content="Skapa en ny annons" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    
-      <div className="container mx-auto">
-        <div className="flex w-full items-center">
-          <h1 className="p-4 font-bold">Skapa Din Annons</h1>
-          <span className="flex-grow border-t border-dashed border-veryDarkBlue opacity-25"></span>
+      <div className="w-full h-screen flex flex-col justify-between items-center font-nunito">
+        <h1 className="font-bold text-2xl text-veryDarkBlue">
+          Skapa ny annons
+        </h1>
+        <div className="bg-veryDarkBlue rounded-md text-center h-28 w-28 flex flex-col items-center justify-center -translate-x-10">
+          <AddIcon />
+          <p className="text-[#FAFAFA] text-sm pt-2">Annonsbild</p>
         </div>
-        <ItemForm categories={categories} />    
+        <ItemForm categories={categories} />
       </div>
-    </>   
+    </>
   );
 };
 
