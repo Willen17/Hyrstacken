0dc2ffae13560b2f1bc1c878203968da0bc744e3
William Saar <williamsaar@outlook.com>
2022-11-29 17:01:43 +0100

    changed design for form and added loader to it

--- components/Forms/ItemForm.tsx
+++ components/Forms/ItemForm.tsx
@@ -1,65 +1,134 @@
-import { zodResolver } from '@hookform/resolvers/zod';
-import React from 'react'
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { itemSchema } from '../../lib/schemas';
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { itemSchema } from "../../lib/schemas";
+import CrossIcon from "../../assets/cross.svg";
+import router from "next/router";
+import Loader from "../Loader/Loader";
 
 type ItemFormProps = {
-  categories: {
-    id: string;
-    name: string;
-  }[];
-}
+    categories: {
+        id: string;
+        name: string;
+    }[];
+};
 
-const ItemForm = ({categories}: ItemFormProps) => {
-
-  const { register, handleSubmit, watch, formState: { errors } } = useForm<z.infer<typeof itemSchema>>({
-    resolver: zodResolver(itemSchema)
-  });
-  const onSubmit = handleSubmit((data) => {
-    console.log(data);
-     fetch("/api/createItem", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
+const ItemForm = ({ categories }: ItemFormProps) => {
+    const [loading, setLoading] = useState(false);
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<z.infer<typeof itemSchema>>({
+        resolver: zodResolver(itemSchema),
     });
-  })
-  return (
-    <form onSubmit={onSubmit} className="form-control flex flex-col p-5">
-      <label className='label'>
-        <span className='label-text'>Bildlänk</span>
-      </label>
-      <input {...register("imageUrl")} />
-      <label className="label">
-        <span className="label-text">Titel</span>
-      </label>
-      <input {...register("title", {required: "Måste ha en titel"})} className="input w-full border-b border-b-veryDarkBlue"/>
-      <label className="input-group input-group-vertical">
-        <span className="label-text">Pris per dag</span>
-        <input type="number" {...register("price", {valueAsNumber: true})} className="input"/>
-      <label className="label">
-        Beskrivning
-      </label>
-      <textarea {...register("description")} className="textarea textarea-bordered max-h-28"/>
-      </label>
-      <label className="label">
-        <select name="" id="" className='select w-full max-w-xs'>
-          <option disabled selected> Välj kategori</option>
-          {categories.map((category) => {
-            return <option {...register("categoryId")} key={category.id} value={category.name}>{category.name}</option>
-          })
-        }</select>
-      </label>
+    const onSubmit = handleSubmit((data) => {
+        console.log(data);
+        setLoading(true);
+        fetch("/api/createItem", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((data) => console.log("response ", data))
+            .catch((e) => console.log(e))
+            .finally(() => {
+                setLoading(false);
+            });
+    });
 
-      {errors && <span>{errors.title?.message}</span>}
-      {errors && <span>{errors.description?.message}</span>}
-      {errors && <span>{errors.price?.message}</span>}
-      
-      <input type="submit" className="btn bg-[#e37e7e] mt-12"/>
-    </form>
-  )
-}
+    const allFilled = () => {
+        let status = true;
+        let { description, categoryId, title, price } = watch();
+        if (!description || !categoryId || !title || !price) status = false;
+        return status;
+    };
 
-export default ItemForm
+    return loading ? (
+        <Loader />
+    ) : (
+        <form
+            onSubmit={onSubmit}
+            className="flex flex-col w-full h-full p-5 justify-evenly tablet:p-10 laptop:w-2/5 tablet:w-3/5 "
+        >
+            <h1 className="text-2xl font-bold text-veryDarkBlue">
+                Skapa ny annons
+            </h1>
+            {allFilled() ? "true" : "false"}
+            <div className="relative flex flex-col my-3 gap-y-3">
+                <label className="label-text">Titel</label>
+                <input
+                    placeholder="Skiftnyckel"
+                    {...register("title", { required: "Måste ha en titel" })}
+                    className="pl-2 pr-10 font-bold border-b-[1px] h-9 border-veryDarkBlue"
+                />
+                <CrossIcon className="absolute right-3  top-[65%] cursor-pointer" />
+            </div>
+
+            <div className="relative flex flex-col my-3 gap-y-3">
+                <label className="label-text">Bild-url</label>
+                <input
+                    {...register("imageUrl")}
+                    placeholder="https://dinbild.se/din-bild"
+                    className="pl-2 pr-10 font-bold border-b-[1px] h-9 border-veryDarkBlue"
+                />
+                <CrossIcon className="absolute right-3  top-[65%] cursor-pointer" />
+            </div>
+            <div className="relative flex flex-col my-3 gap-y-3">
+                <label className="label-text">Pris per dag</label>
+                <input
+                    placeholder="10"
+                    className="pl-2 pr-10 font-bold border-b-[1px] h-9 border-veryDarkBlue"
+                    type="number"
+                    {...register("price", { valueAsNumber: true })}
+                />
+                <CrossIcon className="absolute right-3  top-[65%] cursor-pointer" />
+            </div>
+            <div className="flex flex-col my-3 gap-y-3">
+                <label className="label-text">Beskrivning</label>
+                <textarea
+                    {...register("description")}
+                    className="textarea px-2 border-[1px] font-nunito text-[#000] border-veryDarkBlue leading-snug"
+                    placeholder="Denna skiftnyckel köptes in för tre år sedan och är i mycket bra skick..."
+                    rows={4}
+                />
+            </div>
+            <label className="label-text">Kategori</label>
+            <select
+                name=""
+                id=""
+                className=" select  border-veryDarkBlue border-[1px]"
+            >
+                <option disabled selected>
+                    Välj kategori
+                </option>
+                {categories.map((category) => {
+                    return (
+                        <option
+                            {...register("categoryId")}
+                            key={category.id}
+                            value={category.name}
+                        >
+                            {category.name}
+                        </option>
+                    );
+                })}
+            </select>
+
+            {errors && <span>{errors.title?.message}</span>}
+            {errors && <span>{errors.description?.message}</span>}
+            {errors && <span>{errors.price?.message}</span>}
+
+            <input
+                type="submit"
+                className="my-4 text-white border-0 btn bg-softRed"
+            />
+        </form>
+    );
+};
+
+export default ItemForm;
--- pages/createItem.tsx
+++ pages/createItem.tsx
@@ -2,44 +2,36 @@ import { InferGetServerSidePropsType, NextPage } from "next";
 import prisma from "../lib/prisma";
 import ItemForm from "../components/Forms/ItemForm";
 import Head from "next/head";
-import AddIcon from "../assets/add.svg";
 
 export async function getServerSideProps() {
-  const categories = await prisma.category.findMany({
-    select: {
-      id: true,
-      name: true,
-    },
-  });
-  return {
-    props: {
-      categories,
-    },
-  };
+    const categories = await prisma.category.findMany({
+        select: {
+            id: true,
+            name: true,
+        },
+    });
+    return {
+        props: {
+            categories,
+        },
+    };
 }
 
 const CreateItem: NextPage<
-  InferGetServerSidePropsType<typeof getServerSideProps>
+    InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ categories }) => {
-  return (
-    <>
-      <Head>
-        <title>Hyrstacken - Skapa Annons</title>
-        <meta name="description" content="Skapa en ny annons" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="w-full h-screen flex flex-col justify-between items-center font-nunito">
-        <h1 className="font-bold text-2xl text-veryDarkBlue">
-          Skapa ny annons
-        </h1>
-        <div className="bg-veryDarkBlue rounded-md text-center h-28 w-28 flex flex-col items-center justify-center -translate-x-10">
-          <AddIcon />
-          <p className="text-[#FAFAFA] text-sm pt-2">Annonsbild</p>
-        </div>
-        <ItemForm categories={categories} />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Hyrstacken - Skapa Annons</title>
+                <meta name="description" content="Skapa en ny annons" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="flex flex-col items-center justify-center w-full h-fit smallPhone:h-screen font-nunito">
+                <ItemForm categories={categories} />
+            </div>
+        </>
+    );
 };
 
 export default CreateItem;
