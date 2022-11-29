5726ade7cbe3014f5406c002772c9a91df041945
William Saar <williamsaar@outlook.com>
2022-11-29 09:58:20 +0100

    added weback to use svgs as element

--- next.config.js
+++ next.config.js
@@ -3,10 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    swcPlugins: [
-      'next-superjson-plugin', {}
-    ],
+    swcPlugins: ["next-superjson-plugin", {}],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
