2927a1fe89ae9eac630456bb7aabc723a946040b
matsegard <matsegard@outlook.com>
2022-11-29 15:31:30 +0100

    deleted import

--- pages/playground/test.tsx
+++ pages/playground/test.tsx
@@ -1,16 +1,7 @@
-import ProductCard from "../components/ProductCard/ProductCard";
-
 const Test = () => {
-  return (
-    <div className="h-screen flex justify-center items-center text-8xl text-blue-500 flex-col">
-      {/* <div>
-        <p className="font-nunito font-extralight">
-          Låna verktyg istället för att köpa nytt
-        </p>
-      </div> */}
-      <ProductCard />
-    </div>
-  );
+    return (
+        <div className="h-screen flex justify-center items-center text-8xl text-blue-500 flex-col"></div>
+    );
 };
 
 export default Test;
