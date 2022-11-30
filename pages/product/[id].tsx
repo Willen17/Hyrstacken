fefb92909af181bda79cd2d91dff21ab75bd8388
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-11-30 14:41:01 +0100

    add dynamic paths

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,13 +1,67 @@
 /* eslint-disable @next/next/no-img-element */
-import RatingIcon from "../../assets/productPage/rating.svg";
-import LocationIcon from "../../assets/productPage/location.svg";
-import CalendarIcon from "../../assets/productPage/calendar.svg";
-import ArrowBackIcon from "../../assets/productPage/arrowBack.svg";
-import DatePicker from "react-datepicker";
-import { use, useEffect, useState } from "react";
 import router from "next/router";
+import { GetStaticProps, NextPage } from "next/types";
+import { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import ArrowBackIcon from "../../assets/productPage/arrowBack.svg";
+import CalendarIcon from "../../assets/productPage/calendar.svg";
+import LocationIcon from "../../assets/productPage/location.svg";
+import RatingIcon from "../../assets/productPage/rating.svg";
+import prisma from "../../lib/prisma";
 
-const Product = () => {
+// get static paths from api
+export const getStaticPaths = async () => {
+  const items = await prisma.item.findMany({
+    select: {
+      id: true,
+    },
+  });
+
+  const paths = items.map((item) => ({
+    params: { id: item.id.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+// get static props from api
+
+type Product = {
+  id: number;
+  title: string;
+  description: string;
+  picePerDay: number;
+  imageUrl: string;
+  ownerId: string;
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const {id} = (params as {id: string});
+  const product = await prisma.item.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      picePerDay: true,
+      imageUrl: true,
+      ownerId: true,
+    },
+  });
+  return {
+    props: {
+      product,
+    },
+  };
+};
+
+
+const Product: NextPage<{ product: Product }> = ({product}) => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [dateError, setDateError] = useState<boolean>();
@@ -23,6 +77,7 @@ const Product = () => {
     } else setDateError(false);
   }, [parsedStartDate, parsedEndDate]);
 
+  
   const item = {
     img: "https://images.unsplash.com/photo-1485965120184-e220f721d03e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
     price: 3600,
@@ -41,7 +96,7 @@ const Product = () => {
   };
 
   return (
-    <div className=" w-full h-screen font-nunito overflow-hidden laptop:flex  laptop:items-center">
+    <div className="w-full h-screen overflow-hidden font-nunito laptop:flex laptop:items-center">
       <div className="h-1/2  bg-white laptop:h-full laptop:w-[60%] laptop:flex laptop:justify-center laptop:items-center">
         <div
           onClick={() => router.back()}
@@ -50,58 +105,58 @@ const Product = () => {
           <ArrowBackIcon className="text-white" />
         </div>
         {orderSubmitted ? (
-          <div className="bg-veryDarkBlue flex flex-col justify-center items-center h-full w-full laptop:w-5/6 laptop:h-5/6 laptop:rounded-2xl">
-            <h1 className="font-bold text-2xl text-white">
+          <div className="flex flex-col items-center justify-center w-full h-full bg-veryDarkBlue laptop:w-5/6 laptop:h-5/6 laptop:rounded-2xl">
+            <h1 className="text-2xl font-bold text-white">
               Förfrågan skickad!
             </h1>
-            <p className="text-center text-white w-1/2  text-sm py-2">
+            <p className="w-1/2 py-2 text-sm text-center text-white">
               Vi meddelar dig så fort annonsören svarat. Du ser alla meddelanden
               under dina notiser
             </p>
-            <button className="btn border-softRed border-2 rounded-full bg-transparent mt-10 w-1/2">
+            <button className="w-1/2 mt-10 bg-transparent border-2 rounded-full btn border-softRed">
               TILLBAKA HEM
             </button>
           </div>
         ) : (
           <img
-            src={item.img}
-            alt={item.title}
-            className="object-center object-cover h-full w-full laptop:w-5/6 laptop:h-5/6 laptop:rounded-2xl "
+            src={product.imageUrl || item.img}
+            alt={product.title}
+            className="object-cover object-center w-full h-full laptop:w-5/6 laptop:h-5/6 laptop:rounded-2xl "
           />
         )}
       </div>
       <div className="w-full h-fit rounded-t-2xl  absolute top-[45%] bg-white overflow-hidden laptop:relative laptop:rounded-2xl  laptop:h-5/6 laptop:w-1/3 laptop:top-0 laptop:border-veryDarkBlue/[50%] laptop:border-[1px]">
-        <div className="p-8 flex flex-col justify-evenly h-full overflow-hidden">
-          <h1 className="font-bold text-2xl">{item.title}</h1>
+        <div className="flex flex-col h-full p-8 overflow-hidden justify-evenly">
+          <h1 className="text-2xl font-bold">{product.title}</h1>
           <p className="font-bold">
-            {item.price} SEK
+            {product.picePerDay} SEK
             <span className="font-normal">/dag</span>
           </p>
-          <p className="w-[95%] text-sm py-2">{item.description}</p>
-          <div className="flex flex-wrap gap-x-5  smallPhone:justify-between flex-row text-white text-sm items-center w-full py-5">
-            <div className="flex smallPhone:w-auto my-2 bg-veryDarkBlue rounded-3xl px-3 py-1">
+          <p className="w-[95%] text-sm py-2">{product.description}</p>
+          <div className="flex flex-row flex-wrap items-center w-full py-5 text-sm text-white gap-x-5 smallPhone:justify-between">
+            <div className="flex px-3 py-1 my-2 smallPhone:w-auto bg-veryDarkBlue rounded-3xl">
               <LocationIcon className="self-center " />
               <p className="pl-1">{item.location}</p>
             </div>
-            <div className="flex smallPhone:w-auto my-2 bg-veryDarkBlue  rounded-3xl px-3 py-1">
+            <div className="flex px-3 py-1 my-2 smallPhone:w-auto bg-veryDarkBlue rounded-3xl">
               <RatingIcon className="self-center" />
               <p className="pl-1">{item.score} betyg</p>
             </div>
-            <div className="flex smallPhone:w-auto my-2 bg-veryDarkBlue items-center rounded-3xl px-3 py-1 ">
+            <div className="flex items-center px-3 py-1 my-2 smallPhone:w-auto bg-veryDarkBlue rounded-3xl ">
               <div className="avatar">
-                <div className="rounded-full w-5">
+                <div className="w-5 rounded-full">
                   <img alt="Profile picture" src={item.creatorPic} />
                 </div>
               </div>
               <p className="pl-1">{item.creator}</p>
             </div>
           </div>
-          <div className="flex relative justify-between items-center pb-5">
-            <p className="font-bold whitespace-nowrap mr-2">Boka produkt</p>
+          <div className="relative flex items-center justify-between pb-5">
+            <p className="mr-2 font-bold whitespace-nowrap">Boka produkt</p>
             <div className="bg-[#26324540] w-full h-px" />
           </div>
 
-          <div className="flex w-full  font-bold justify-between">
+          <div className="flex justify-between w-full font-bold">
             <p>Hämta:</p>
             <div className="flex  w-[6rem]">
               <CalendarIcon className="absolute -translate-x-5" />
@@ -114,7 +169,7 @@ const Product = () => {
               />
             </div>
           </div>
-          <div className="flex w-full font-bold justify-between py-5">
+          <div className="flex justify-between w-full py-5 font-bold">
             <p>Lämna:</p>
             <div className="flex  w-[6rem]">
               <CalendarIcon className="absolute -translate-x-5" />
@@ -127,13 +182,13 @@ const Product = () => {
               />
             </div>
           </div>
-          <p className="text-error text-xs">
+          <p className="text-xs text-error">
             {touched &&
               dateError &&
               "Vänligen fyll i ett korrekt datum. Startdatum kan inte vara efter slutdatum."}
           </p>
 
-          <div className="card-actions justify-center pt-5">
+          <div className="justify-center pt-5 card-actions">
             <button
               className={`btn text-white rounded-full font-bold tracking-widest w-full border-0 bg-softRed ${
                 dateError && "btn-disabled opacity-50"
