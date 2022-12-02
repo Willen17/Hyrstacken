3dd908992b172851699b253f287783d92e1155d4
William Saar <williamsaar@outlook.com>
2022-12-02 16:20:30 +0100

    api endpoint for edit profile

---
+++ pages/api/editProfile.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { z } from "zod";
+import prisma from "../../lib/prisma";
+import { profileSchema } from "../../lib/schemas";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const session = await getSession({ req });
+
+    if (req.method !== "POST") {
+        return res.status(405).end();
+    }
+
+    if (!session?.user?.email) {
+        return res.status(401).end();
+    }
+
+    const { name, image } = await profileSchema.parseAsync(req.body);
+
+    const updatedUser = await prisma.user.update({
+        where: { email: session.user.email },
+        data: {
+            name,
+            image,
+        },
+    });
+
+    res.status(200).json({ id: updatedUser.id });
+}
