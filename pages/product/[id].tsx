1676310f59f49de6a4fd613a3b0136f3b8f3fdcd
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-12-05 17:47:16 +0100

    optimized form, updated schema, propet typs for profilepage and productpage

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import router from "next/router";
-import { GetStaticProps, NextPage } from "next/types";
+import { GetStaticProps, NextPage, InferGetStaticPropsType, GetStaticPropsContext } from "next/types";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import ArrowBackIcon from "../../assets/productPage/arrowBack.svg";
@@ -29,18 +29,13 @@ export const getStaticPaths = async () => {
 };
 
 // get static props from api
-
-export type Product = {
-    id: number;
-    title: string;
-    description: string;
-    picePerDay: number;
-    imageUrl: string;
-    ownerId: string;
-};
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const { id } = params as { id: string };
+export const getStaticProps = async ({ params }: GetStaticPropsContext<{ id: string }>) => {
+    if (!params) {
+        return {
+            notFound: true,
+        };
+    }
+    const { id } = params;
     const product = await prisma.item.findUnique({
         where: {
             id,
@@ -51,9 +46,22 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
             description: true,
             picePerDay: true,
             imageUrl: true,
-            ownerId: true,
+            owner: {
+                select: {
+                    id: true,
+                    name: true,
+                    image: true,
+                }
+            }
         },
     });
+
+    if (!product) {
+        return {
+            notFound: true,
+        };
+    }
+
     return {
         props: {
             product,
@@ -62,7 +70,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     };
 };
 
-const Product: NextPage<{ product: Product }> = ({ product }) => {
+const Product: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ product }) => {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
     const [dateError, setDateError] = useState<boolean>();
@@ -138,17 +146,17 @@ const Product: NextPage<{ product: Product }> = ({ product }) => {
                             <RatingIcon className="self-center" />
                             <p className="pl-1">{item.score} betyg</p>
                         </div>
-                        <Link href={`/profile/${product.ownerId}`}>
+                        <Link href={`/profile/${product.owner.id}`}>
                             <div className="flex items-center px-3 py-1 my-2 bg-veryDarkBlue rounded-3xl ">
                                 <div className="avatar">
                                     <div className="w-5 rounded-full">
                                         <img
                                             alt="Profile picture"
-                                            src={item.creatorPic}
+                                            src={product.owner.image || item.creatorPic}
                                         />
                                     </div>
                                 </div>
-                                <p className="pl-1">{item.creator}</p>
+                                <p className="pl-1">{product.owner.name || 'Anonym användare'}</p>
                             </div>
                         </Link>
                     </div>
