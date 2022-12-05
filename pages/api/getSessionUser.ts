8a0160c26edcfea77adc4cc0d98ccf1039f583a6
William Saar <williamsaar@outlook.com>
2022-12-05 14:08:04 +0100

    created endpoint to get a user id form a session

---
+++ pages/api/getSessionUser.ts
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { z } from "zod";
+import prisma from "../../lib/prisma";
+import { profileSchema } from "../../lib/schemas";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const session = await getSession({ req });
+
+    if (!session?.user?.email) {
+        return res.status(401).end();
+    }
+
+    const userId = await prisma.user.findUnique({
+        where: { email: session.user.email },
+    });
+
+    res.status(200).json({ id: userId?.id });
+}
