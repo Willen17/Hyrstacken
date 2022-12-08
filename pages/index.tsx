ed0ab54e0b5458fcca760d617df6780ce712fa98
William Saar <williamsaar@outlook.com>
2022-12-08 11:06:11 +0100

    moved chnage name modal to layout conponent

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,30 +3,23 @@ import Image from "next/image";
 import Typewriter from "typewriter-effect";
 
 import heroImg from "../public/assets/hero-img.jpg";
+import Logo from "../public/assets/logo.png";
+import PussleImg from "../public/assets/pussle-img.png";
 import TrainImage from "../public/assets/train-2.png";
 import TrainMob from "../public/assets/train-mob.png";
-import PussleImg from "../public/assets/pussle-img.png";
-import Logo from '../public/assets/logo.png';
 
-import FilterIcon from "../public/assets/filter-icon.svg";
 import ArrowRightIcon from "../public/assets/arrow-right.svg";
+import FilterIcon from "../public/assets/filter-icon.svg";
 import SearchIcon from "../public/assets/search-icon.svg";
 
+import Link from "next/link";
+import { useEffect, useState } from "react";
 import PrimaryButton from "../components/PrimaryButton/PrimaryButton";
 import SecondaryButton from "../components/PrimaryButton/SecondaryButton";
-import { Fragment, useEffect, useState } from "react";
-import Link from "next/link";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
 
 let isFirstLoad: boolean = true;
 
 export default function Home() {
-    const router = useRouter()
-    const session = useSession()
-    const [id, setId] = useState<string>();
-    const [fireRequestModal, setFireRequestModal] = useState<boolean>(false)
-
     function useWindowWidth() {
         const [windowWidth, setWindowWidth] = useState<number | undefined>(
             undefined
@@ -48,40 +41,6 @@ export default function Home() {
 
     const windowWidth = useWindowWidth();
 
-    useEffect(() => {
-        fetch("/api/getSessionUser", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-        .then(async (data) => {
-            const user = await data.json();
-            setId(user.id);
-        })
-        .catch((e) => console.log(e));
-    }) 
-
-    const useRequestModal = () => {
-        useEffect(() => {
-            const handler = () => {
-                if(isFirstLoad && session && !session.data?.user?.name && id) {
-                    isFirstLoad = false
-                    setFireRequestModal(true)
-                }
-            }
-            if (document.readyState === "complete") {
-                handler();
-            } else {
-                window.addEventListener('load', handler);
-                return () => document.removeEventListener('load', handler);
-            }
-        })
-        return fireRequestModal
-    }
-
-    useRequestModal()
-
     return (
         <div className="overflow-y-hidden">
             <Head>
@@ -92,12 +51,18 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+
             <main className="overflow-x-hidden">
                 <section className="flex justify-center w-screen h-screen">
-                    {windowWidth && windowWidth < 800
-                    ? <Image src={Logo} alt="logotype" className="absolute top-[2rem] left-[2rem]"/>
-                    : ''
-                    }
+                    {windowWidth && windowWidth < 800 ? (
+                        <Image
+                            src={Logo}
+                            alt="logotype"
+                            className="absolute top-[2rem] left-[2rem]"
+                        />
+                    ) : (
+                        ""
+                    )}
                     <Image
                         src={heroImg}
                         alt="heroImage"
@@ -264,21 +229,6 @@ export default function Home() {
                     </div>
                 </section>
             </main>
-            { fireRequestModal ? 
-            <Fragment>
-                <div className="absolute left-0 top-0 h-full w-full bg-blackish bg-opacity-80" />
-                <div className="absolute top-[45%] min-[460px]:top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full p-8 max-w-[20rem] min-[460px]:max-w-[25rem] min-[630px]:max-w-[30rem] min-[730px]:max-w-[40rem] min-[930px]:max-w-[50rem] h-full max-h-[30rem] rounded-lg bg-white flex gap-6 flex-col justify-center items-center">
-                    <h2 className="text-3xl min-[460px]:text-4xl min-[730px]:text-5xl text-center font-cabin font-bold text-softRed">Hej {session.data?.user?.email}</h2>
-                    <p className="max-w-lg text-center">Sätt prägel på din profil med ett nickname och en bild, och öka chanserna att få hyra andras prylar och hyra ut dina egna.</p>
-                    <div className="flex flex-col min-[460px]:flex-row gap-5">
-                        <PrimaryButton onClick={() => router.push(`/profile/${id}`)}>Ja tack!</PrimaryButton>
-                        <SecondaryButton onClick={() => setFireRequestModal(false)}>Senare</SecondaryButton>
-                    </div>
-                </div>
-            </Fragment>
-            :
-                ''
-            }
         </div>
     );
 }
