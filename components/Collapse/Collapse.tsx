b9aa4a10aa1418cda8c87fb695f96f7ddb86c2ba
William Saar <williamsaar@outlook.com>
2022-12-12 12:15:21 +0100

    made collapse reusable

--- components/Collapse/Collapse.tsx
+++ components/Collapse/Collapse.tsx
@@ -5,26 +5,31 @@ type Props = {
     title: string;
     children: ReactNode;
     length: number;
+    disabled: boolean;
 };
 
-const Collapse = ({ children, title, length }: Props) => {
+const Collapse = ({ children, title, length, disabled }: Props) => {
     const [open, toggleOpen] = useState(false);
     return (
         <div className="flex flex-col items-center justify-between px-2 pb-5 mt-10 ">
             <div
-                onClick={() => toggleOpen(!open)}
-                className="flex items-center w-full pr-2 text-xl font-medium cursor-pointer "
+                onClick={() => !disabled && toggleOpen(!open)}
+                className={`flex items-center w-full pr-2 text-xl font-medium ${
+                    !disabled && "cursor-pointer"
+                }`}
             >
                 <p className="mr-2 font-bold whitespace-nowrap">
                     {title}
                     <span>({length})</span>
                 </p>
                 <div className="bg-[#26324540]  flex-1 h-px" />
-                <Chevron
-                    className={`self-center  ${
-                        open ? "-rotate-90" : "rotate-90"
-                    } ml-3  fill-veryDarkBlue `}
-                />
+                {!disabled && (
+                    <Chevron
+                        className={`self-center  ${
+                            open ? "-rotate-90" : "rotate-90"
+                        } ml-3  fill-veryDarkBlue `}
+                    />
+                )}
             </div>
 
             {open && <>{children}</>}
