78dcf19f0b2fddc661640afa07f04b402a1005ce
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-12-14 15:27:29 +0100

    fix layout

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -1,6 +1,6 @@
 const Loader = () => {
     return (
-        <div className="flex flex-col font-bold text-center gap-y-5">
+        <div className="flex items-center justify-center flex-col font-bold text-center gap-y-5">
             <div className="mr-3 radial-progress animate-spin [--value:40]" />
             <p>Laddar...</p>
         </div>
