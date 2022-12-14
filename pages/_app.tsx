e418208a0e0a473bec8e21eebd54b51d1b0f32fc
matsegard <matsegard@outlook.com>
2022-12-14 09:50:48 +0100

    Add test code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Layout from "../components/Layout/Layout";
+console.log("test");
 
 export default function App({
     Component,
